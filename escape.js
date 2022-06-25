603fd997c25ea98825cdd9589e1b31b8a2ac5706
Yoonsoo Kim <laptise@live.jp>
2022-06-26 01:02:11 +0900

    escape room

--- escape.js
+++ escape.js
@@ -1,32 +1,50 @@
+const input = `
+############.##
+###############
+############.##
+############.##
+############.##
+############.##
+############.##
+############.##
+#######.####.##
+##....#.####..#
+#####.#.####.##
+#####S#.####.##
+#####.#.####.##
+#####........##
+###############
+`;
+
 class Cell {
-  constructor(colIndex, rowIndex) {
+  constructor(rowIndex, colIndex, cellType) {
     this.colIndex = colIndex;
     this.rowIndex = rowIndex;
+    this.isWall = cellType === "#";
   }
   //隣接セル取得
   getAdjacent(debug = false) {
-    const ci = this.room[this.colIndex + 1][this.rowIndex] || null;
-    const cd = this.room[this.colIndex - 1][this.rowIndex] || null;
-    const ri = this.room[this.colIndex][this.rowIndex + 1] || null;
-    const rd = this.room[this.colIndex][this.rowIndex - 1] || null;
+    const ci = this.room[this.rowIndex + 1]?.[this.colIndex] || null;
+    const cd = this.room[this.rowIndex - 1]?.[this.colIndex] || null;
+    const ri = this.room[this.rowIndex]?.[this.colIndex + 1] || null;
+    const rd = this.room[this.rowIndex]?.[this.colIndex - 1] || null;
 
-    debug && console.log({ from: `${this.colIndex},${this.rowIndex}`, ci, cd, ri, rd });
+    debug && console.log({ from: `${this.rowIndex},${this.colIndex}`, ci, cd, ri, rd });
     return [ci, cd, ri, rd].filter((x) => x === null || !x.isWall);
   }
 }
 
 function buildRoom(roomInfo, cellInfo) {
-  const [height, width] = roomInfo.split(" ").map(Number);
+  const [height, width] = roomInfo;
   let startPoint = [];
-  const room = new Array(height).fill(null).map((col, colIndex) => {
-    const row = new Array(width).fill(null).map((row, rowIndex) => {
-      const currentCellType = cellInfo[colIndex][rowIndex];
-      if (currentCellType === "S") startPoint = [colIndex, rowIndex];
-      return new Cell(colIndex, rowIndex);
+  const room = new Array(height).fill(null).map((col, rowIndex) => {
+    const row = new Array(width).fill(null).map((row, colIndex) => {
+      const currentCellType = cellInfo[rowIndex][colIndex];
+      if (currentCellType === "S") startPoint = [rowIndex, colIndex];
+      return new Cell(rowIndex, colIndex, currentCellType);
     });
     return row;
   });
-  console.log(room);
   room.forEach((col) => col.forEach((rol) => (rol.room = room)));
   return [room, ...startPoint];
 }
@@ -37,7 +55,7 @@ function parseCellInfo(cols) {
 
 class Player {
   get historyPos() {
-    return this.history.map((x) => `${x.colIndex},${x.rowIndex}`);
+    return this.history.map((x) => `${x.rowIndex},${x.colIndex}`);
   }
   get currentCell() {
     return this.history[this.history.length - 1];
@@ -47,72 +65,42 @@ class Player {
     this.history = history;
   }
 
-  async tryEscape() {
-    // console.log(
-    //   "trying, now is " +
-    //     `${this.currentCell.colIndex},${this.currentCell.rowIndex}\n from ${this.history.map((x) => `(${x.colIndex},${x.rowIndex})`)}`
-    // );
-    const isEscaped = await new Promise(async (res, rej) => {
-      const adjacentList = this.currentCell.getAdjacent();
-      const candidates =
-        adjacentList?.filter((cell) => {
-          const isNewCell = !this.history.includes(cell);
-          if (isNewCell) {
-            return true;
-          } else {
-            return false;
-          }
-        }) || [];
-      if (candidates.length === 0) {
-        return res(false);
+  escape() {
+    try {
+      const res = this.crawl();
+      return res === null;
+    } catch (e) {
+      if (e === "escaped") {
+        console.log("IIII!!!");
+        return true;
       } else {
-        const nextTries = await Promise.all(
-          candidates.map(async (cell, index) => {
-            if (cell === null) {
-              //   console.log(`${this.currentCell.colIndex},${this.currentCell.rowIndex}`);
-              this.currentCell.getAdjacent(true);
-              return true;
-            } else if (index === 0) {
-              this.history.push(cell);
-              return await this.tryEscape();
-            } else {
-              return await new Player(this.room, [...this.history, cell]).tryEscape();
-            }
-          })
-        );
-        const nextTryRes = nextTries?.find?.((x) => x === true);
-        if (nextTryRes) {
-          //   console.log(nextTries);
-          //   console.log(this.historyPos);
-          //   console.log(`escaped from ${this.currentCell.colIndex},${this.currentCell.rowIndex}`);
-          res(nextTryRes);
-        } else {
-          res(false);
-        }
+        console.error(e);
       }
-    });
-    return isEscaped;
+    }
+  }
+
+  crawl() {
+    if (this.currentCell === null) {
+      return null;
+    }
+    const adjacents = this.currentCell.getAdjacent().filter((cell) => !this.history.includes(cell));
+    if (adjacents.find((x) => x === null)) {
+      throw "escaped";
+    } else {
+      // console.log(`adjacent of ${rowIndex},${colIndex}`, adjacents);
+      const newPlayes = adjacents.map((x) => new Player(this.room, [...this.history, x]).crawl());
+      return newPlayes.find((x) => x === null);
+    }
   }
 }
 
-async function main(lines) {
-  console.clear();
-  const [roomInfo, ...cellInfo] = lines;
-  const [room, startCol, startRow] = buildRoom(roomInfo, parseCellInfo(cellInfo));
-  const player = new Player(room, [room[startCol][startRow]]);
-  const isEscaped = await player.tryEscape();
+async function main(cellInfo) {
+  const roomInfo = [cellInfo.length, Array.from(cellInfo[0]).length];
+  const [room, startRow, startCol] = buildRoom(roomInfo, parseCellInfo(cellInfo));
+  const player = new Player(room, [room[startRow][startCol]]);
+  const isEscaped = player.escape();
   console.log(isEscaped ? "YES" : "NO");
   //  console.log(room[8][8]);
 }
-const input = `
-##########
-.....#####
-##########
-#.S#.#####
-#.##.#####
-#....#####
-#....#####
-#........#
-##########
-##########`;
-main(input.split("\n"));
+
+main(input.split("\n").filter(Boolean));
